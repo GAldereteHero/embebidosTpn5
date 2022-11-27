f7209146cf9e5f320dc28b956610f2f0d448ab40
GAldereteHero <herogonzalo@gmail.com>
2022-11-27 20:54:09 -0300

    Se implementan test de verificar 1s y 10s

--- test/test_reloj.c
+++ test/test_reloj.c
@@ -5,22 +5,55 @@
 
 #define TICKS_PER_SECOND 5
 
+clock_t reloj;
+
+/**
+ * @brief Funcion que Ceedling ejecuta antes de realizar cada una de las pruebas
+ * 
+ */
+void setUp(void){
+    static const uint8_t INICIAL[] = {1,2,3,4};
+    reloj = ClockCreate(TICKS_PER_SECOND);
+    ClockSetupTime(reloj, INICIAL, sizeof(INICIAL));
+}
+
 // Configurar la libreria, consultar la hora tiene que ser invalida.
-void test_hora_inicial_invalida(void){
+void test_start_up(void){
     static const uint8_t ESPERADO[] = {0,0,0,0,0,0};
     uint8_t hora[6];
-    clock_t reloj = ClockCreate(TICKS_PER_SECOND); // Recibe por params la cant. de pulsos de reloj que valen 1 segundo
+    reloj = ClockCreate(TICKS_PER_SECOND); // Recibe por params la cant. de pulsos de reloj que valen 1 segundo
     TEST_ASSERT_FALSE( ClockGetTime(reloj, hora, sizeof(hora)));
     TEST_ASSERT_EQUAL_UINT8_ARRAY( ESPERADO, hora, sizeof(ESPERADO));
 }
 
 // Configurar la libreria, ajustar la hora (con valores correctos), consultar la hora y tiene que ser valida.
 void test_set_up_current_time(void){
-    static const uint8_t INICIAL[] = {1,2,3,4};
     static const uint8_t ESPERADO[] = {1,2,3,4,0,0};
     uint8_t hora[6];
-    clock_t reloj = ClockCreate(TICKS_PER_SECOND);
-    ClockSetupTime(reloj, INICIAL, sizeof(INICIAL));
     TEST_ASSERT_TRUE( ClockGetTime(reloj, hora, sizeof(hora)));
     TEST_ASSERT_EQUAL_UINT8_ARRAY( ESPERADO, hora, sizeof(ESPERADO));
+}
+
+// Simular el paso de n ciclos de reloj, consultar la hora y verificar que avanzo un segundo 
+void test_one_second_elapsed(void){
+    static const uint8_t ESPERADO[] = {1,2,3,4,0,1};
+    uint8_t hora[6];
+
+    for (int i = 0; i < TICKS_PER_SECOND; i++){
+        ClockNewTick(reloj);
+    }
+    ClockGetTime(reloj, hora, sizeof(hora));
+    TEST_ASSERT_EQUAL_UINT8_ARRAY(ESPERADO, hora, sizeof(ESPERADO));
+}
+
+// Simular el paso de n ciclos de reloj, consultar la hora y verificar que avanzo diez segundos 
+void test_ten_second_elapsed(void){
+    static const uint8_t ESPERADO[] = {1,2,3,4,1,0};
+    uint8_t hora[6];
+
+    for (int i = 0; i < 10 * TICKS_PER_SECOND; i++){
+        ClockNewTick(reloj);
+    }
+    ClockGetTime(reloj, hora, sizeof(hora));
+    TEST_ASSERT_EQUAL_UINT8_ARRAY(ESPERADO, hora, sizeof(ESPERADO));
 }
